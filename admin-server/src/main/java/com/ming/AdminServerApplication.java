c82e34833f9c4b05e132f6e0d22421b0381372fa
mingcanchen <mingcanchen@hotmail.com>
2018-07-06 11:00:38 +0800

    账户配置

--- admin-server/src/main/java/com/ming/AdminServerApplication.java
+++ admin-server/src/main/java/com/ming/AdminServerApplication.java
@@ -3,6 +3,7 @@ package com.ming;
 import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
